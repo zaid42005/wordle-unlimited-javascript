0be6ed754c40f60c1e627626f3216a587c34835b
zaidahmed <zaid.42005@gmail.com>
2023-10-02 03:43:09 +0400

    added input for 0,0

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,8 +1,16 @@
+import { useContext } from "react";
 import React from 'react'
+import { AppContext } from "../App";
 
 function Key({keyVal, bigKey}) {
+    const {board ,setBoard} =  useContext(AppContext);
+    const selectLetter  = () => {
+        const newBoard = [...board]
+        newBoard[0][0] = keyVal
+        setBoard(newBoard)
+    }
   return (
-    <div className='key' id={bigKey && "big"}>{keyVal}</div>
+    <div className='key' id={bigKey && "big"} onClick={selectLetter}>{keyVal}</div>
   )
 }
 

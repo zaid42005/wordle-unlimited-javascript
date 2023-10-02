10cfa26a8d39bf8938cec9ef9abc11f6f8ee8976
zaidahmed <zaid.42005@gmail.com>
2023-10-02 07:03:07 +0400

    added document event listener

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -5,6 +5,16 @@ function Keyboard() {
     const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
     const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
     const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
+    useEffect(()=>{
+        document.addEventListener("keydown", handleKeyboard)
+        return () => {
+            document.removeEventListener("keydown", handleKeyboard)
+        }
+    }, [handleKeyboard])
+
+    }
+
   return (
     <div className='keyboard'>
         <div className='line1'>

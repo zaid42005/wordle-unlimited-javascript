13945660fc1ecc60d055b5f09fad06ea3ccae128
zaidahmed <zaid.42005@gmail.com>
2023-10-02 07:09:45 +0400

    created handleKeyboard using useCallBack(event) to handle the events for enter backspace and any other keys its an important hook and it maintains performance because it stops reupdating components unnessicarily

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import Key from './Key'
 
 function Keyboard() {
     const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
     const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
     const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
+    const handleKeyboard = useCallback((event) =>{
+        if (event.key === "Enter"){
+
+        } else if(event.key === "Backspace"){
+
+        }
+        else{
+            
+        }
+    })
 
     useEffect(()=>{
         document.addEventListener("keydown", handleKeyboard)
@@ -16,7 +27,7 @@ function Keyboard() {
     }
 
   return (
-    <div className='keyboard'>
+    <div className='keyboard' onKeyDown={handleKeyboard}>
         <div className='line1'>
             {keys1.map((key) => {
             return <Key keyVal = {key} />

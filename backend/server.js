df0d344e2919a109d70fd4ca184659070a492b4d
Zaid <66367657+zaid42005@users.noreply.github.com>
2024-04-03 01:22:23 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.get('/random-word', (req, res) => {
     console.log("Random word:", randomWord);
 
     // Send the random word as the response
-    res.json({ word: randomWord });
+    res.json({ word: randomWord, words });
 });
 
 // Start the server
